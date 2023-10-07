f63e30d6c8bd9282fd2592d3636eebdb0ca781c6
TuNisiAa <federico.toluzzo1@gmail.com>
2023-10-07 10:57:09 +0200

    Added method "cmd(int n)" that once given a number as a parameter keeps asking for a string until the string given is "exit" (in which case true is returned) or the loop is executed more than n times (in which case false is returned).

--- src/me/tunisiaa/Main.java
+++ src/me/tunisiaa/Main.java
@@ -1,9 +1,12 @@
 package me.tunisiaa;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	    count(1, 15);
+        System.out.println(cmd(5));
     }
 
     public static void count(int n, int m) {
@@ -11,5 +14,17 @@ public class Main {
         if(n != m){
             count(n + 1, m);
         }
+    }
+
+    public static boolean cmd(int n){
+        String val;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < n; i++){
+            val = sc.nextLine();
+            if(val.equals("exit")){
+                return true;
+            }
+        }
+        return false;
     }
 }
