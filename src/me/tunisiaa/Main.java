4c7f439ab8453dba8494f66c603d65a36e84fa5f
TuNisiAa <federico.toluzzo1@gmail.com>
2023-10-07 10:46:17 +0200

    Added recursive method "count()" that once called writes all numbers between the parameters n and m included. Modified the method removing the unnecessary return statement and instead simply using the difference operator.

--- src/me/tunisiaa/Main.java
+++ src/me/tunisiaa/Main.java
@@ -3,15 +3,15 @@ package me.tunisiaa;
 public class Main {
 
     public static void main(String[] args) {
-	    count(5, 150);
+	    count(1, 15);
     }
 
     public static void count(int n, int m) {
         System.out.println(n);
-        if(n == m){
-            return;
-        }else{
+        if(n != m){
             count(n + 1, m);
         }
     }
+
+
 }
