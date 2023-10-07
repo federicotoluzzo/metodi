dfa02d7b95019d79ca1536823034a8ef0d94f3b4
TuNisiAa <federico.toluzzo1@gmail.com>
2023-10-07 11:35:06 +0200

    Added the hundred() method.

--- src/me/tunisiaa/Main.java
+++ src/me/tunisiaa/Main.java
@@ -8,6 +8,8 @@ public class Main {
 	    //count(1, 15);
         //System.out.println(cmd(5));
         //System.out.println(recursiveCMD(5));
+        //System.out.println(multiples(5, 10));
+        hundred();
     }
 
     public static void count(int n, int m) {
@@ -42,6 +44,7 @@ public class Main {
         return recursiveCMD(n - 1);
     }
 
+//    Esercizio 9
 //    Progettare un algoritmo che, dato un valore nu-
 //    merico k, legga n numeri e conti quanti di questi
 //    sono multipli di k scrivendo il risultato.
@@ -57,5 +60,36 @@ public class Main {
             }
         }
         return multiples;
+    }
+
+//    Esercizio 10
+//    Progettare un algoritmo che legga una sequenza
+//    di valori numerici fino a che la loro somma è mi-
+//    nore di 100 e scriva la somma ottenuta e quanti
+//    sono i valori letti.
+
+    public static void hundred(){
+        int val = 0;
+        int sum = 0;
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        while(sum + val < 100){
+            sum += val;
+            val = sc.nextInt();
+            i++;
+        }
+        System.out.println("The sum of the " + (i - 1) + " numbers given is " + sum);
+    }
+
+/*  Esercizio 11
+    I Progettare un algoritmo che, dati due valori nu-
+    merici h e k, legga n valori e conti quanti di essi
+    sono compresi tra h e k scrivendo il risultato.
+*/
+    public static int between(int h, int k, int n){
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < n; i++){
+
+        }
     }
 }
