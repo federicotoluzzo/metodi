fbc7ad1990c3dcedbdd36afef2844ebfb5f52460
TuNisiAa <federico.toluzzo1@gmail.com>
2023-10-07 10:30:55 +0200

    Added recursive method "count()" that once called writes all numbers between the parameters n and m included.

--- src/me/tunisiaa/Main.java
+++ src/me/tunisiaa/Main.java
@@ -3,6 +3,15 @@ package me.tunisiaa;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    count(5, 150);
+    }
+
+    public static void count(int n, int m) {
+        System.out.println(n);
+        if(n == m){
+            return;
+        }else{
+            count(n + 1, m);
+        }
     }
 }
