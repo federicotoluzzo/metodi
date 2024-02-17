4ed626206932925e059bd14abb8a0a6804893003
tunisiaa-guy <federico.toluzzo1@gmail.com>
2024-02-17 10:40:15 +0100

    GUI missing

--- src/me/tunisiaa/classi/es4/Item.java
+++ src/me/tunisiaa/classi/es4/Item.java
@@ -22,4 +22,44 @@ public class Item {
                              "Quantity : %d\n" +
                              "ID : %d", name, description, price, quantity, id);
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/me/tunisiaa/classi/es4/Storage.java
+++ src/me/tunisiaa/classi/es4/Storage.java
@@ -1,19 +1,57 @@
 package me.tunisiaa.classi.es4;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Storage {
-    private ArrayList<Item> items;
+    private HashSet<Item> items;
     private int id = 1;
 
     public Storage(){
-        items = new ArrayList<Item>();
+        items = new HashSet<Item>();
     }
 
     public void addItem(String name, String description, float price, int quantity){
         items.add(new Item(name, description, price, quantity, id++));
     }
 
+    public boolean removeItem(int id){
+        for(Item i : items){
+            if(i.getId() == id){
+                items.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getItem(int id){
+        for(Item i : items){
+            if(i.getId() == id){
+                return i.toString();
+            }
+        }
+        return "";
+    }
+
+    public boolean editItemQuantity(int id, int quantity){
+        for(Item i : items){
+            if(i.getId() == id){
+                i.setQuantity(quantity);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean editItemPrice(int id, float price){
+        for(Item i : items){
+            if(i.getId() == id){
+                i.setPrice(price);
+                return true;
+            }
+        }
+        return false;
+    }
 
     public String toString(){
         String s = "- - - - - - - - - -\n";
