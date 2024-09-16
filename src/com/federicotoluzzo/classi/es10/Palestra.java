6b0fae2521fa56fe14e221d248a9d417a6c97833
tunisiaa-guy <federico.toluzzo1@gmail.com>
2024-09-16 14:43:56 +0200

    Aggiunto documentazione Palestra.java

--- src/com/federicotoluzzo/classi/es10/Palestra.java
+++ src/com/federicotoluzzo/classi/es10/Palestra.java
@@ -15,6 +15,8 @@ public class Palestra{
         utenti = new ArrayList<>();
     }
 
+    /* ricerca, probabilmente sostituito da stringaUtente()
+
     public Utente getUtente(String cf){
         for(Utente u : utenti){
             if(u.getCodiceFiscale() == cf){
@@ -22,7 +24,13 @@ public class Palestra{
             }
         }
         return null;
-    }
+    }*/
+
+    /**
+     *
+     * @param utente utente da aggiungere alla palestra
+     * @return vero se l'utente è stato aggiunto, altrimenti falso
+     */
 
     public boolean aggiungi(Utente utente){
         for(Utente u : utenti){
@@ -34,6 +42,13 @@ public class Palestra{
         return true;
     }
 
+    /**
+     *
+     * @param cf codice fiscale dell'utente da modificare
+     * @param abbonamento abbonamento con cui sostituire il precedente
+     * @return vero se l'utente viene modificato, falso altrimenti
+     */
+
     public boolean modificaAbbonamento(String cf, Abbonamento abbonamento){
         for(Utente u : utenti){
             if(u.getCodiceFiscale() == cf){
@@ -43,6 +58,13 @@ public class Palestra{
         }
         return false;
     }
+
+    /**
+     *
+     * @param cf codice fiscale dell'utente da modificare
+     * @param nuovoNome nome e cognome con cui sostituire i precedenti
+     * @return vero se l'utente viene modificato, falso altrimenti
+     */
 
     public boolean modificaNomeCognome(String cf, String nuovoNome){
         for(Utente u : utenti){
@@ -55,6 +77,12 @@ public class Palestra{
         return false;
     }
 
+    /**
+     *
+     * @param cf codice fiscale dell'utente da eliminare
+     * @return vero se l'utente è stato eliminato, falso altrimenti
+     */
+
     public boolean elimina(String cf){
         for(Utente u : utenti){
             if(u.getCodiceFiscale() == cf){
@@ -65,6 +93,12 @@ public class Palestra{
         return false;
     }
 
+    /**
+     *
+     * @param cf codice fiscale dell'utente da visualizzare
+     * @return una stringa contenente i dati dell'utente con il codice fiscale cf
+     */
+
     public String stringaUtente(String cf){
         for(Utente u : utenti){
             if(u.getCodiceFiscale() == cf){
@@ -73,6 +107,11 @@ public class Palestra{
         }
         return "Utente non trovato";
     }
+
+    /**
+     *
+     * @return I dati di tutti gli utenti
+     */
 
     public String stringaUtenti(){
         String stringaUtenti = "";
