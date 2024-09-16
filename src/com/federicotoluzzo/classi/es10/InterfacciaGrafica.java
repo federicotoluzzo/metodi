97e6dcc32afd2f79611b3874feaa59c697bfb67e
tunisiaa-guy <federico.toluzzo1@gmail.com>
2024-09-16 14:50:23 +0200

    Aggiunto documentazione InterfacciaGrafica.java

--- src/com/federicotoluzzo/classi/es10/InterfacciaGrafica.java
+++ src/com/federicotoluzzo/classi/es10/InterfacciaGrafica.java
@@ -6,18 +6,26 @@ public class InterfacciaGrafica {
     public Palestra palestra;
 
     public static final String PROMPT =
-            "|\tMenu principale\n|\n" +
-            "|\t1)\tAggiungi utente\n" +
-            "|\t2)\tRimuovi utente\n" +
-            "|\t3)\tModifica nome e cognome utente\n" +
-            "|\t4)\tModifica abbonamento utente\n" +
-            "|\t5)\tStampa utente\n" +
-            "|\t6)\tStampa tutti gli utenti\n|\n" +
-            "|\t0)\tTermina il programma\n";
+            """
+            |\tMenu principale
+            |
+            |\t1)\tAggiungi utente
+            |\t2)\tRimuovi utente
+            |\t3)\tModifica nome e cognome utente
+            |\t4)\tModifica abbonamento utente
+            |\t5)\tStampa utente
+            |\t6)\tStampa tutti gli utenti
+            |
+            |\t0)\tTermina il programma
+            """;
 
     public InterfacciaGrafica() {
         palestra = new Palestra();
     }
+
+    /**
+     * mostra le opzioni e sceglie correttamente gli input
+     */
 
     public void start(){
         while(true){
@@ -36,6 +44,10 @@ public class InterfacciaGrafica {
             }
         }
     }
+
+    /**
+     * chiede nella riga di comando i dati e aggiunge un'utente con essi alla palestra
+     */
     private void aggiungiUtente(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Scrivi il nome e il cognome dell'utente da aggiungere, separati da uno spazio : ");
@@ -60,6 +72,10 @@ public class InterfacciaGrafica {
         palestra.aggiungi(new Utente(nome.split(" ")[0], nome.split(" ")[1], codiceFiscale, abbonamento));
     }
 
+    /**
+     * chiede nella riga di comando un codice fiscale e permette di modificare l'abbonamento dell'utente
+     */
+
     private void modificaAbbonamento(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Scrivi il codice fiscale dell'utente di cui modificare l'abbonamento : ");
@@ -81,6 +97,10 @@ public class InterfacciaGrafica {
         }
     }
 
+    /**
+     * chiede un codice fiscale e permette di modificare il nome dell'utente
+     */
+
     private void modificaNomeCognome(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Scrivi il codice fiscale dell'utente di cui modificare il nome e cognome : ");
@@ -92,6 +112,10 @@ public class InterfacciaGrafica {
         }
     }
 
+    /**
+     * chiede un codice fiscale nella riga di comando e permette di eliminare un'utente
+     */
+
     public void elimina(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Scrivi il codice fiscale dell'utente da eliminare : ");
@@ -101,6 +125,9 @@ public class InterfacciaGrafica {
         }
     }
 
+    /**
+     * chiede un codice fiscale e stampa i dati dell'utente al terminale
+     */
 
     public void stampaUtente(){
         Scanner sc = new Scanner(System.in);
@@ -108,6 +135,10 @@ public class InterfacciaGrafica {
         String codiceFiscale = sc.nextLine();
         System.out.println(palestra.stringaUtente(codiceFiscale));
     }
+
+    /**
+     * stampa tutti gli utenti
+     */
 
     public void stampaTutto(){
         System.out.println(palestra.stringaUtenti());
