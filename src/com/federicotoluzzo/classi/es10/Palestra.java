83881cb55975f5e2e6150a37a7e6a209eeb4605d
federico.toluzzo <federico.toluzzo1@gmail.com>
2024-09-16 12:07:11 +0200

    asd

---
+++ src/com/federicotoluzzo/classi/es10/Palestra.java
@@ -0,0 +1,8 @@
+package com.federicotoluzzo.classi.es10;
+
+import java.util.ArrayList;
+
+public class Palestra{
+    private ArrayList<Utente> Utenti;
+    private double superficie;
+}
---
+++ src/com/federicotoluzzo/classi/es10/Utente.java
@@ -0,0 +1,7 @@
+package com.federicotoluzzo.classi.es10;
+
+public class Utente {
+
+/*    I metodi della classe Utente sono:
+            -costruttore, i metodi get, i metodi set, stampa*/
+}
