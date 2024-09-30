0c44a312dbbe2a3a0bba0a008d61f828be767ee3
federico.toluzzo <federico.toluzzo1@gmail.com>
2024-09-30 12:05:48 +0200

    Crazy? I was crazy once! They put me in a room, a rubber room, a rubber room with rats, and rats make me crazy! Crazy? 4 anni di superiori e ancora a fare interfacce a riga di comando 🗣️🗣️🗣️Crazy? I was crazy once! They put me in a room, a rubber room, a rubber room with rats, and rats make me crazy! Crazy?

---
+++ src/com/federicotoluzzo/classi/quickSort/Main.java
@@ -0,0 +1,24 @@
+package com.federicotoluzzo.classi.quickSort;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> ArrayList;
+        ArrayList<Integer> a = ArrayList<>(new int[]{3, 1, 2, 6, 4, 5, 9, 10, 8});
+        System.out.println(a.toString());
+        System.out.println(quickSort(a, 0, a.length));
+    }
+
+    public static int[] quickSort(int[] arr) {
+        if(arr.length == 0) return arr;
+        
+        int pivot = arr[0];
+        int[] arr1 = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < pivot){
+                
+            }
+        }
+    }
+}
