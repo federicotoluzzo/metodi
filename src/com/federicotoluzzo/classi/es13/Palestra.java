725b4840de3f0217daf327d62ee9b7b1b28c11fa
federico.toluzzo <federico.toluzzo1@gmail.com>
2024-10-28 11:21:10 +0100

    es 13

---
+++ src/com/federicotoluzzo/classi/es13/Palestra.java
@@ -0,0 +1,109 @@
+package com.federicotoluzzo.classi.es13;
+
+import java.util.ArrayList;
+
+public class Palestra{
+    enum Abbonamento{
+        GOLD,
+        SILVER,
+        BRONZE,
+        TRIAL
+    }
+    private ArrayList<Utente> utenti;
+
+    public Palestra(){
+        utenti = new ArrayList<>();
+    }
+
+    public int ricerca(String cf){
+        for(int i = 1){
+            if(u.getCodiceFiscale() == cf){
+                return u;
+            }
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param utente utente da aggiungere alla palestra
+     * @return vero se l'utente è stato aggiunto, altrimenti falso
+     */
+
+    public boolean aggiungi(Utente utente){
+        for(Utente u : utenti){
+            if(u.getCodiceFiscale() == utente.getCodiceFiscale()){
+                return false;
+            }
+        }
+        utenti.add(utente);
+        return true;
+    }
+
+    /**
+     *
+     * @param cf codice fiscale dell'utente da modificare
+     * @param nuovoNome nome e cognome con cui sostituire i precedenti
+     * @return vero se l'utente viene modificato, falso altrimenti
+     */
+
+    public boolean modificaNomeCognome(String cf, String nuovoNome){
+        for(Utente u : utenti){
+            if(u.getCodiceFiscale() == cf){
+                u.setNome(nuovoNome.split(" ")[0]);
+                u.setCognome(nuovoNome.split(" ")[1]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param cf codice fiscale dell'utente da eliminare
+     * @return vero se l'utente è stato eliminato, falso altrimenti
+     */
+
+    public boolean elimina(String cf){
+        for(Utente u : utenti){
+            if(u.getCodiceFiscale() == cf){
+                utenti.remove(u);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param cf codice fiscale dell'utente da visualizzare
+     * @return una stringa contenente i dati dell'utente con il codice fiscale cf
+     */
+
+    public String stringaUtente(String cf){
+        for(Utente u : utenti){
+            if(u.getCodiceFiscale() == cf){
+                System.out.println(u.toString());
+            }
+        }
+        return "Utente non trovato";
+    }
+
+    /**
+     *
+     * @return I dati di tutti gli utenti
+     */
+
+    public String stringaUtenti(){
+        String stringaUtenti = "";
+        for(Utente u : utenti){
+            stringaUtenti += u.toString() + "\n\n";
+        }
+        return stringaUtenti;
+    }
+}
+/*
+I metodi della classe Palestra sono:
+-costruttore, ricerca, aggiungi, modifica, elimina, stampaUtente, stampaTutti.
+-ritornaTesta, ritornaCoda, isEmpty, numeroElementi, ritornaElemento (in base alla posizione).
+*/
---
+++ src/com/federicotoluzzo/classi/es13/Utente.java
@@ -0,0 +1,61 @@
+package com.federicotoluzzo.classi.es13;
+import com.federicotoluzzo.classi.es10.Palestra.Abbonamento;
+
+public class Utente {
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+
+    /**
+     *
+     * @param nome nome dell'utente
+     * @param cognome cognome dell'utente
+     * @param codiceFiscale codice fiscale dell'utente
+     */
+
+    public Utente(String nome, String cognome, String codiceFiscale) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    /**
+     *
+     * @return tutti i dati dell'utente in forma di stringa formattata
+     */
+
+    public String toString() {
+        String res = "Nome: " + nome + "\n";
+        res += "Cognome: " + cognome + "\n";
+        res += "CodiceFiscale: " + codiceFiscale + "\n";
+        res += "\n";
+        return res;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    /*    I metodi della classe Utente sono:
+            -costruttore, i metodi get, i metodi set, stampa*/
+}
