272c54defbff405290d900f3d7078a44a5fbd6a9
federico.toluzzo <federico.toluzzo1@gmail.com>
2025-01-13 11:48:15 +0100

    CLI conclusa

--- src/com/federicotoluzzo/classi/irlanda/Irlanda.java
+++ src/com/federicotoluzzo/classi/irlanda/Irlanda.java
@@ -1,6 +1,8 @@
 package com.federicotoluzzo.classi.irlanda;
 
+import java.util.Scanner;
 import java.util.Stack;
+import java.util.concurrent.TimeUnit;
 
 public class Irlanda {
     private String[] città = {"Dublino", "Belfast", "Galway", "Cork", "Waterford", "Londonderry", "Limerick"};
@@ -15,9 +17,26 @@ public class Irlanda {
     };
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Irlanda ir = new Irlanda();
-        ir.dijkstra(0);
-        ir.printDistance(5, 3);
+        while(true){
+            for (int i = 0; i < ir.città.length; i++) {
+                System.out.printf("%s : %d\n", ir.città[i], i);
+            }
+            System.out.println("Tra che città vuoi calcolare il percorso? Indica i numeri separati da uno spazio");
+            String dati = sc.nextLine();
+            int start = Integer.parseInt(dati.split(" ")[0]);
+            int end = Integer.parseInt(dati.split(" ")[1]);
+            System.out.println();
+            ir.printDistance(start, end);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println();
+            System.out.println();
+        }
     }
 
     public void printDistance(int start, int end){
@@ -48,8 +67,6 @@ public class Irlanda {
             }
             if (min == -1) {break;}
             nodi[min] = "V";
-            System.out.println("ho visitato nodo: "+min);
-            //aggiorno peso dei nodi adiacenti
             for (int i = 0; i < distanze.length; i = i + 1) {
                 if ((distanze[min][i] != 0) && (nodi[i].equals("NV")) && (pesi[min] + distanze[min][i] < pesi[i])) {
                     pesi[i] = pesi[min] + distanze[min][i];
@@ -57,64 +74,21 @@ public class Irlanda {
                 }
             }
         }
-        System.out.println("vettore u:");
-        for(int i=0;i<u.length;i=i+1){
-            System.out.println("predecessore-padre del nodo " + i + " nell'ALB dei cammini minimi è il nodo: " + u[i]);
-        }
-        int padre = u[end];
+
+        System.out.printf("Distanza tra %s e %s : %d\n", città[start], città[end], pesi[end]);
+
+        int padre = end;
         Stack<String> path = new Stack<>();
         while(padre != -1){
             path.push(città[padre]);
-            distanza += pesi[padre];
             padre = u[padre];
         }
+        System.out.print("Percorso : ");
+        System.out.print(path.pop());
         while(!path.isEmpty()){
-            System.out.println(path.pop());
+            System.out.print(" -> ");
+            System.out.print(path.pop());
         }
-
-        System.out.printf("distanza tra %s e %s : %d\n", città[start], città[end], distanza);
-    }
-
-    public void dijkstra (int n){
-        //trova il cammino di lunghezza minima partendo da nodo n verso tutti gli altri nodi del grafo
-        String [] nodi=new String[this.distanze.length];
-        int [] u=new int[this.distanze.length];  //vettore u
-        for(int i=0;i<u.length;i=i+1){
-            u[i]=-1;
-        }
-        for(int i=0;i<nodi.length;i=i+1){
-            nodi[i]="NV";
-        }
-        int [] pesi=new int[this.distanze.length];
-        for(int i=0;i<pesi.length;i=i+1){
-            pesi[i]=1000000;
-        }
-        pesi[n]=0;
-        while(true) {
-            int min = -1;
-            for (int i = 0; i < pesi.length; i = i + 1) {
-                if ((min == -1) && (nodi[i].equals("NV"))) {
-                    min = i;
-                } else {
-                    if ((nodi[i].equals("NV"))&&(pesi[i] < pesi[min])) {
-                        min = i;
-                    }
-                }
-            }
-            if (min == -1) {break;}
-            nodi[min] = "V";
-            System.out.println("ho visitato nodo: "+min);
-            //aggiorno peso dei nodi adiacenti
-            for (int i = 0; i < distanze.length; i = i + 1) {
-                if ((distanze[min][i] != 0) && (nodi[i].equals("NV")) && (pesi[min] + distanze[min][i] < pesi[i])) {
-                    pesi[i] = pesi[min] + distanze[min][i];
-                    u[i]=min;
-                }
-            }
-        }
-        System.out.println("vettore u:");
-        for(int i=0;i<u.length;i=i+1){
-            System.out.println("predecessore-padre del nodo " + i + " nell'ALB dei cammini minimi è il nodo: " + u[i]);
-        }
+        System.out.println();
     }
 }
