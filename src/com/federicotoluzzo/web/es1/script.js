41ef609e3b3f58799a353850eee274ffa0fea9ee
federico.toluzzo <federico.toluzzo1@gmail.com>
2025-01-31 13:46:58 +0100

    web

--- src/com/federicotoluzzo/web/es1/script.js
+++ src/com/federicotoluzzo/web/es1/script.js
@@ -4,3 +4,13 @@ setInterval(function () {
     localStorage.setItem("deg", (parseInt(localStorage.getItem("deg")) + 1).toString());
     ruota.style.transform = "rotate(" + localStorage.getItem("deg") + "deg)";
 }, 10);
+document.addEventListener("mousemove", function (mouse) {
+    var item = document.getElementById("perspective");
+    var rect = item.getBoundingClientRect();
+    var centerX = rect.x + rect.width / 2;
+    var centerY = rect.y + rect.height / 2;
+    var deltaX = mouse.x - centerX;
+    var deltaY = mouse.y - centerY;
+    console.log("perspective(1000px) rotateX(".concat(deltaX, ") rotateY(").concat(deltaY, ")"));
+    item.style.transform = "perspective(1000px) rotateX(".concat(deltaX, ") rotateY(").concat(deltaY, ")");
+});
--- src/com/federicotoluzzo/web/es1/script.ts
+++ src/com/federicotoluzzo/web/es1/script.ts
@@ -5,3 +5,16 @@ setInterval(() => {
     localStorage.setItem("deg", (parseInt(localStorage.getItem("deg")) + 1).toString());
     ruota.style.transform = "rotate(" + localStorage.getItem("deg") + "deg)";
 }, 10);
+
+document.addEventListener("mousemove", (mouse) =>  {
+    const item = document.getElementById("perspective");
+    const rect = item.getBoundingClientRect()
+    const centerX = rect.x + rect.width / 2;
+    const centerY = rect.y + rect.height / 2;
+
+    const deltaX = mouse.x - centerX;
+    const deltaY = mouse.y - centerY;
+
+    console.log(`perspective(1000px) rotateX(${deltaX}) rotateY(${deltaY})`)
+    item.style.transform = `perspective(1000px) rotateX(${deltaX}) rotateY(${deltaY})`;
+});
