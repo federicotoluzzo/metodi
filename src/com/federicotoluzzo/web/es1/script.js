5ae0a435eae7ad02fc460119e5a528e3161859e8
federico.toluzzo <federico.toluzzo1@gmail.com>
2025-02-03 12:00:24 +0100

    parallax

--- src/com/federicotoluzzo/web/es1/script.js
+++ src/com/federicotoluzzo/web/es1/script.js
@@ -5,12 +5,44 @@ setInterval(function () {
     ruota.style.transform = "rotate(" + localStorage.getItem("deg") + "deg)";
 }, 10);
 document.addEventListener("mousemove", function (mouse) {
-    var item = document.getElementById("perspective");
+    var item = document.getElementById("perspective1");
     var rect = item.getBoundingClientRect();
     var centerX = rect.x + rect.width / 2;
     var centerY = rect.y + rect.height / 2;
     var deltaX = mouse.x - centerX;
     var deltaY = mouse.y - centerY;
-    console.log("perspective(1000px) rotateX(".concat(deltaX, ") rotateY(").concat(deltaY, ")"));
-    item.style.transform = "perspective(1000px) rotateX(".concat(deltaX, ") rotateY(").concat(deltaY, ")");
+    //const rotY = deltaX > 0 ? Math.log(deltaX) * 2 : Math.log(-deltaX) * -2;
+    //const rotX = deltaY > 0 ? Math.log(deltaY) * -2 : Math.log(-deltaY) * 2;
+    var rotX = deltaY / 50;
+    var rotY = deltaX / -50;
+    item.style.transformStyle = "preserve-3d";
+    item.style.transform = "perspective(1000px) rotateX(".concat(rotX.toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
+});
+document.addEventListener("mousemove", function (mouse) {
+    var item = document.getElementById("perspective2");
+    var rect = item.getBoundingClientRect();
+    var centerX = rect.x + rect.width / 2;
+    var centerY = rect.y + rect.height / 2;
+    var deltaX = mouse.x - centerX;
+    var deltaY = mouse.y - centerY;
+    //const rotY = deltaX > 0 ? Math.log(deltaX) * 2 : Math.log(-deltaX) * -2;
+    //const rotX = deltaY > 0 ? Math.log(deltaY) * -2 : Math.log(-deltaY) * 2;
+    var rotX = deltaY / 50;
+    var rotY = deltaX / -50;
+    item.style.transformStyle = "preserve-3d";
+    item.style.transform = "perspective(750px) rotateX(".concat(rotX.toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
+});
+document.addEventListener("mousemove", function (mouse) {
+    var item = document.getElementById("perspective3");
+    var rect = item.getBoundingClientRect();
+    var centerX = rect.x + rect.width / 2;
+    var centerY = rect.y + rect.height / 2;
+    var deltaX = mouse.x - centerX;
+    var deltaY = mouse.y - centerY;
+    //const rotY = deltaX > 0 ? Math.log(deltaX) * 2 : Math.log(-deltaX) * -2;
+    //const rotX = deltaY > 0 ? Math.log(deltaY) * -2 : Math.log(-deltaY) * 2;
+    var rotX = deltaY / 50;
+    var rotY = deltaX / -50;
+    item.style.transformStyle = "preserve-3d";
+    item.style.transform = "perspective(500px) rotateX(".concat(rotX.toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
 });
--- src/com/federicotoluzzo/web/es1/script.ts
+++ src/com/federicotoluzzo/web/es1/script.ts
@@ -7,14 +7,47 @@ setInterval(() => {
 }, 10);
 
 document.addEventListener("mousemove", (mouse) =>  {
-    const item = document.getElementById("perspective");
+    const item = document.getElementById("perspective1");
     const rect = item.getBoundingClientRect()
     const centerX = rect.x + rect.width / 2;
     const centerY = rect.y + rect.height / 2;
 
     const deltaX = mouse.x - centerX;
     const deltaY = mouse.y - centerY;
+    //const rotY = deltaX > 0 ? Math.log(deltaX) * 2 : Math.log(-deltaX) * -2;
+    //const rotX = deltaY > 0 ? Math.log(deltaY) * -2 : Math.log(-deltaY) * 2;
+    const rotX = deltaY / 50;
+    const rotY = deltaX / -50;
+    item.style.transformStyle = "preserve-3d"
+    item.style.transform = "perspective(1000px) rotateX(".concat(rotX.toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
+});
+document.addEventListener("mousemove", (mouse) =>  {
+    const item = document.getElementById("perspective2");
+    const rect = item.getBoundingClientRect()
+    const centerX = rect.x + rect.width / 2;
+    const centerY = rect.y + rect.height / 2;
 
-    console.log(`perspective(1000px) rotateX(${deltaX}) rotateY(${deltaY})`)
-    item.style.transform = `perspective(1000px) rotateX(${deltaX}) rotateY(${deltaY})`;
+    const deltaX = mouse.x - centerX;
+    const deltaY = mouse.y - centerY;
+    //const rotY = deltaX > 0 ? Math.log(deltaX) * 2 : Math.log(-deltaX) * -2;
+    //const rotX = deltaY > 0 ? Math.log(deltaY) * -2 : Math.log(-deltaY) * 2;
+    const rotX = deltaY / 50;
+    const rotY = deltaX / -50;
+    item.style.transformStyle = "preserve-3d"
+    item.style.transform = "perspective(750px) rotateX(".concat(rotX.toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
+});
+document.addEventListener("mousemove", (mouse) =>  {
+    const item = document.getElementById("perspective3");
+    const rect = item.getBoundingClientRect()
+    const centerX = rect.x + rect.width / 2;
+    const centerY = rect.y + rect.height / 2;
+
+    const deltaX = mouse.x - centerX;
+    const deltaY = mouse.y - centerY;
+    //const rotY = deltaX > 0 ? Math.log(deltaX) * 2 : Math.log(-deltaX) * -2;
+    //const rotX = deltaY > 0 ? Math.log(deltaY) * -2 : Math.log(-deltaY) * 2;
+    const rotX = deltaY / 50;
+    const rotY = deltaX / -50;
+    item.style.transformStyle = "preserve-3d"
+    item.style.transform = "perspective(500px) rotateX(".concat(rotX.toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
 });
